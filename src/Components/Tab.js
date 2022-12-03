e790e433b211405c44902733dc6b1e0d423f9dc7
Ashish Patel <ashishpatel3946@gmail.com>
2022-12-04 02:48:53 +0530

    k

--- src/Components/Tab.js
+++ src/Components/Tab.js
@@ -5,7 +5,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
 import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+ import Box from "@material-ui/core/Box";
 import News from "./news";
 import Social from "./socialmedia";
 import Joke from "./joke";
